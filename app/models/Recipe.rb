baf3023864c95a28e8bc929df696203f9e8b8b7a
Grey <r627137@TRG514104.local>
2016-01-21 14:13:19 +0400

    refactor, improve layout, add new and refacto sort

--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -4,6 +4,8 @@ class Recipe < ActiveRecord::Base
   has_many :stars
   belongs_to :contributor
 
+  attr_reader
+
   def rating
     rating = 0
     if self.stars.any?
@@ -11,5 +13,19 @@ class Recipe < ActiveRecord::Base
     end
     rating
   end
+
+  def self.custom_sort
+    custom_count = Recipe.count > 5 ? 5 : Recipe.count - 2
+    ordered_recipes = self.all.sort_by{ |recipe| recipe.created_at }
+    new_recipes = ordered_recipes.pop(custom_count)
+    best_recipes = ordered_recipes.sort_by{ |recipe| -recipe.rating }
+    new_and_best = best_recipes.zip(new_recipes).flatten
+  end
+
+  def self.new_recipes
+    ordered_recipes = self.all.sort_by{ |recipe| recipe.created_at }
+    new_recipes = ordered_recipes.pop(5)
+  end
+
 end
 
